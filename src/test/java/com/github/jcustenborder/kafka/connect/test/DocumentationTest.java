e5734a2a6469d2b6ee9c93c617a5b833e3735157
raul <rvasqz86@gmail.com>
2023-10-03 10:58:37 -0500

    init

---
+++ src/test/java/com/github/jcustenborder/kafka/connect/test/DocumentationTest.java
@@ -0,0 +1,7 @@
+package com.github.jcustenborder.kafka.connect.test;
+
+import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
+
+public class DocumentationTest extends BaseDocumentationTest {
+
+}
